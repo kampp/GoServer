90f0cc80be051a6d820920f089d5ef3ee2829077
Hendrik Schmidt <hfrschmidt@gmail.com>
2017-03-05 23:48:34 +0100

    updated database setup file

--- doc/database_setup.sql
+++ doc/database_setup.sql
@@ -13,12 +13,10 @@
 
 
 -- Exportiere Datenbank Struktur für server_data
-DROP DATABASE IF EXISTS `server_data`;
 CREATE DATABASE IF NOT EXISTS `server_data` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
 USE `server_data`;
 
 -- Exportiere Struktur von Tabelle server_data.match_requested
-DROP TABLE IF EXISTS `match_requested`;
 CREATE TABLE IF NOT EXISTS `match_requested` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `token` text COLLATE utf8_bin NOT NULL,
@@ -27,11 +25,10 @@ CREATE TABLE IF NOT EXISTS `match_requested` (
   `player_name` varchar(50) COLLATE utf8_bin NOT NULL,
   `rank` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='match_requested holds every client app that wants to match to an opponent. It contains the firebase-token, the game meta information and timestamps for possible timeout, if no match is found';
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='match_requested holds every client app that wants to match to an opponent. It contains the firebase-token, the game meta information and timestamps for possible timeout, if no match is found';
 
 -- Daten Export vom Benutzer nicht ausgewählt
 -- Exportiere Struktur von Tabelle server_data.move_nodes
-DROP TABLE IF EXISTS `move_nodes`;
 CREATE TABLE IF NOT EXISTS `move_nodes` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `gameID` int(11) DEFAULT NULL,
@@ -43,11 +40,10 @@ CREATE TABLE IF NOT EXISTS `move_nodes` (
   `comment` varchar(200) COLLATE utf8_bin DEFAULT NULL,
   `isPrisoner` int(11) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- Daten Export vom Benutzer nicht ausgewählt
 -- Exportiere Struktur von Tabelle server_data.running_games
-DROP TABLE IF EXISTS `running_games`;
 CREATE TABLE IF NOT EXISTS `running_games` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `created_at` timestamp NULL DEFAULT NULL,
@@ -57,7 +53,7 @@ CREATE TABLE IF NOT EXISTS `running_games` (
   `prisonerA` int(11) DEFAULT NULL,
   `prisonerB` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- Daten Export vom Benutzer nicht ausgewählt
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
